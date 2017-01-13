a24cd8ee380516733cd3fb4d8c685ac9bcfcadcc
Michael.VARA <Michael.VARA@SC-C236-PC06>
2017-01-13 08:51:54 +0100

    Story 34 -Ajout d'une classe de test pour la classe Map

---
+++ TestMap/TestMap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pacman
+{
+    [TestClass]
+    public class TestMap
+    {
+        string nomMapExpected;
+        Map _classMap;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            nomMapExpected = "Map01";
+            _classMap = new Map(nomMapExpected);
+        }
+
+        [TestMethod]
+        public void TestCreerMapSuccess()
+        {
+            Assert.AreNotEqual(null, _classMap.map);
+        }
+
+        [TestMethod]
+        public void TestNbPiecesSuccess()
+        {
+            Assert.AreNotEqual(0, _classMap.NbPiece());
+        }
+
+        [TestMethod]
+        public void TestNbMursSuccess()
+        {
+            Assert.AreNotEqual(0, _classMap.NbMurs());
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+            nomMapExpected = null;
+            _classMap = null;
+        }
+    }
+}
