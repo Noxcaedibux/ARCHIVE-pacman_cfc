11ab4af3c917fdf6a10adc14165bb2128b388cb0
diogo <diogo.vieira-ferreira@cpnv.ch>
2017-01-14 09:53:01 +0100

    story 1 ouverture de la base de données creation de la table pour les scores possibilité d'y écrire dedans.

---
+++ Pacman/BaseDeDonnees.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Pacman
+{
+    /// <summary>
+    /// classe qui permettra d'enregistrer nos scores et de les lire
+    /// </summary>
+    public class BaseDeDonnees
+    {
+        #region private attributes
+        private SQLiteConnection _dbConnection;
+        private SQLiteCommand _command;
+        #endregion private attributes
+
+        #region constructors
+        public void CreateDataBase(string name)
+        {
+            name = "MyDatabase.sqlite";
+            _dbConnection = new SQLiteConnection("Data Source="+name+";Version=3;");
+            string sql = "create table highScores (name varchar(20), score int)";
+            _command = new SQLiteCommand(sql, _dbConnection);
+            _command.ExecuteNonQuery();
+        }
+        #endregion constructors 
+
+        #region accessors and mutators
+        #endregion accessors and mutators
+
+        #region public methods
+        public void OpenDataBase()
+        {
+            _dbConnection.Open();
+        }
+        public void WriteInDbTable(string playerName, int score)
+        {
+            string sql = "insert into highScores (name, score) values ('"+playerName+"', "+score+")";
+            _command = new SQLiteCommand(sql, _dbConnection);
+            _command.ExecuteNonQuery();
+        }
+        public void CloseDataBase()
+        {
+            _dbConnection.Close();
+        }
+        #endregion public methods
+
+        #region private methods
+        #endregion private methods
+    }
+}
---
+++ SQLite/TestSqLiteManager.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Data.SQLite;
+using System.Collections.Generic;
+
+namespace SQLite
+{
+    [TestClass]
+    public class TestSQLiteManager
+    {
+        #region private attributes
+        private SQLiteManager _sqLiteMgr;
+        private string _databaseNameExpected;
+        private string _databaseNameActual;
+        #endregion private attributes
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this._databaseNameExpected = "MyDatabase.sqlite";
+            this._sqLiteMgr = new SQLiteManager(this._databaseNameExpected);
+        }
+
+        [TestMethod]
+        public void SqliteManager_DatabaseName_AfterInitialization_GetDatabaseName()
+        {
+            //given
+            //refere to Initialize()
+
+            //when
+            this._databaseNameActual = this._sqLiteMgr.DatabaesName;
+
+            //then
+            Assert.AreEqual(this._databaseNameExpected, this._databaseNameActual);
+        }
+
+        [TestMethod]
+        public void SqliteManager_Constructor_AfterInitialization_DatabaseExists()
+        {
+            //given
+            //refere to Initialize()
+
+            //when
+            //the constructor create the database
+
+            //then
+            Assert.IsTrue(File.Exists(this._databaseNameExpected));
+        }
+
+        [TestMethod]
+        public void SqliteManager_DatabaseState_WhenOpened_StateIsOpen()
+        {
+            //given
+            //refere to Initialize()
+
+            //when
+            this._sqLiteMgr.OpenConnection();
+
+            //then
+            Assert.IsTrue(this._sqLiteMgr.DatabaseState == System.Data.ConnectionState.Open);
+        }
+
+        [TestMethod]
+        public void SqliteManager_DatabaseState_WhenClosed_StateIsClosed()
+        {
+            //given
+            //refere to Initialize()
+            this._sqLiteMgr.OpenConnection();
+
+            //when
+            this._sqLiteMgr.CloseConnection();
+
+            //then
+            Assert.IsTrue(this._sqLiteMgr.DatabaseState == System.Data.ConnectionState.Closed);
+        }
+
+        [TestMethod]
+        public void SQLiteManager_QueryCreateTable_QueryCorrect_TableCreated()
+        {
+            //init
+            string tableName = "ANIMALS";
+            string queryCreateTable = "CREATE TABLE " + tableName + " (species varchar(20), quantity int)";
+            string queryCreateValidation = "SELECT COUNT(name) FROM sqlite_master WHERE type='table' AND name='" + tableName + "'";
+            int queryResultExpected = 1;
+            int queryResultActual = -1;
+
+            //given
+            //refere to Initialize()
+            this._sqLiteMgr.QueryCreateTable(queryCreateTable);
+
+            //when
+            queryResultActual = this._sqLiteMgr.QueryCount(queryCreateValidation);
+
+            //then
+            Assert.AreEqual(queryResultExpected, queryResultActual);
+        }
+
+        [TestMethod]
+        public void SQLiteManager_QueryInsert_QueryCorrect_ValueInserted()
+        {
+            //init
+            string tableName = "ANIMALS";
+            string queryCreateTable = "CREATE TABLE " + tableName + " (species varchar(20), quantity int)";
+            string queryInsert = "INSERT INTO " + tableName + " (species, quantity) values ('Elephant', 24)";
+            string queryInsertValidation = "SELECT COUNT(species) FROM " + tableName;
+            int queryResultExpected = 1;
+            int queryResultActual = -1;
+
+            //given
+            //refere to Initialize()
+            this._sqLiteMgr.QueryCreateTable(queryCreateTable);
+            this._sqLiteMgr.QueryInsert(queryInsert);
+
+            //when
+            queryResultActual = this._sqLiteMgr.QueryCount(queryInsertValidation);
+
+            //then
+            Assert.AreEqual(queryResultExpected, queryResultActual);
+        }
+
+        [TestMethod]
+        public void SQLiteManager_QuerySelect_QueryCorrect_ValuesGet()
+        {
+            //init
+            string tableName = "ANIMALS";
+            string attribut1 = "species";
+            string attribut2 = "quantity";
+            string queryCreateTable = "CREATE TABLE " + tableName + " ("+ attribut1 + " varchar(20), " + attribut2 + " int)";
+            string queryInsert = "INSERT INTO " + tableName + " (" + attribut1 + ", " + attribut2 + ") values ('Elephant', 24)";
+            string querySelect = "SELECT " + attribut1 + ", " + attribut2 + " FROM " + tableName;
+            string queryResultExpected = "Elephant,24";
+            string queryResultActual = "";
+            List<string> listOfAttributs = new List<string>();
+            listOfAttributs.Add(attribut1);
+            listOfAttributs.Add(attribut2);
+
+            //given
+            //refere to Initialize()
+            this._sqLiteMgr.QueryCreateTable(queryCreateTable);
+            this._sqLiteMgr.QueryInsert(queryInsert);
+
+
+            //when
+            List<string> querySelectResult = this._sqLiteMgr.QuerySelect(querySelect, listOfAttributs);
+            
+            //then
+            Assert.AreEqual(queryResultExpected, querySelectResult[0]);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            if (this._sqLiteMgr.DatabaseState != System.Data.ConnectionState.Closed)
+            {
+                this._sqLiteMgr.CloseConnection();              
+            }
+
+            SQLiteConnection.ClearAllPools();
+
+            if (File.Exists(this._databaseNameExpected))
+            {
+                File.Delete(this._databaseNameExpected);
+            }
+            this._sqLiteMgr = null;
+        }
+    }
+}
