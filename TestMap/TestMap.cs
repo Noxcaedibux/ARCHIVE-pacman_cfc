989af0f36e6573933328e1d0afef3a5630521c62
diogo <diogo.vieira-ferreira@cpnv.ch>
2017-01-15 15:11:51 +0100

    story 55 FINAL POUR L'EXAM
    ajout des xml

--- TestMap/TestMap.cs
+++ TestMap/TestMap.cs
@@ -8,32 +8,44 @@ namespace Pacman
     {
         string nomMapExpected;
         Map _classMap;
-
+        /// <summary>
+        /// Initialisement de nos variables
+        /// </summary>
         [TestInitialize]
         public void TestInit()
         {
             nomMapExpected = "Map01";
             _classMap = new Map(nomMapExpected);
         }
-
+        /// <summary>
+        /// on va tester que notre map retourne une
+        /// valeur qui n'est pas nulle
+        /// </summary>
         [TestMethod]
         public void TestCreerMapSuccess()
         {
             Assert.AreNotEqual(null, _classMap.map);
         }
-
+        /// <summary>
+        /// on va tester que notre map nous retourne bien
+        /// une valeur de pièces
+        /// </summary>
         [TestMethod]
         public void TestNbPiecesSuccess()
         {
             Assert.AreNotEqual(0, _classMap.NbPiece());
         }
-
+        /// <summary>
+        /// on regarde qu'on ait bien des murs
+        /// </summary>
         [TestMethod]
         public void TestNbMursSuccess()
         {
             Assert.AreNotEqual(0, _classMap.NbMurs());
         }
-
+        /// <summary>
+        /// on remet à zero nos variables pour le prochain test
+        /// </summary>
         [TestCleanup]
         public void TestClean()
         {
